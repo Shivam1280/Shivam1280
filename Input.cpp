42aa8ccf0b0172c947a9fddbce1846032c19c425
Shivam1280 <141243712+Shivam1280@users.noreply.github.com>
2023-08-02 18:41:13 +0530

    Create Input.cpp
    C++ Program

---
+++ Input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    // Input from the user
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    // Check if the number is even or odd
+    if (number % 2 == 0) {
+        std::cout << "Number is even." << std::endl;
+    } else {
+        std::cout << "Number is odd." << std::endl;
+    }
+
+    return 0;
+}
+
